850b9f7fc125bd363de3d0dbc6501197adb9afe8
lixiaokai <lixiaokai@szzbmy.com>
2017-11-21 18:56:30 +0800

    x

--- bin-debug/GameView.js
+++ bin-debug/GameView.js
@@ -19,50 +19,6 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
     }
     return t;
 };
-var betBtnListConfig = [
-    {
-        name: 'shizi',
-        times: 'x15',
-        resource: 'game_c1_png',
-        default: 'game_c1_png',
-        active: 'game_c1_active_png'
-    },
-    {
-        name: 'houzi',
-        times: 'x10',
-        resource: 'game_c2_png',
-        default: 'game_c2_png',
-        active: 'game_c2_active_png'
-    },
-    {
-        name: 'banma',
-        times: 'x6',
-        resource: 'game_c3_png',
-        default: 'game_c3_png',
-        active: 'game_c3_active_png'
-    },
-    {
-        name: 'luotuo',
-        times: 'x6',
-        resource: 'game_c4_png',
-        default: 'game_c4_png',
-        active: 'game_c4_active_png'
-    },
-    {
-        name: 'tuoniao',
-        times: 'x4',
-        resource: 'game_c5_png',
-        default: 'game_c5_png',
-        active: 'game_c5_active_png'
-    },
-    {
-        name: 'yezhu',
-        times: 'x4',
-        resource: 'game_c6_png',
-        default: 'game_c6_png',
-        active: 'game_c6_active_png'
-    }
-];
 function mapWithIndex(arr, merge) {
     return arr.map(function (item, index) {
         return __assign({}, item, merge, { index: "" + index });
@@ -71,11 +27,27 @@ function mapWithIndex(arr, merge) {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+var BET_AMOUNT = [1, 10, 20, 50, 100];
+var INIT_BET_AMOUNT = 50;
+var INIT_TOTAL_COIN = 1000;
+var MAX_BET_COIN = 100;
+var GAME_TIME = 5000;
 var GameView = (function (_super) {
     __extends(GameView, _super);
     function GameView() {
         var _this = _super.call(this) || this;
+        // 游戏对象
         _this.roles = [];
+        // 游戏数据
+        _this.betBtnListConfig = [];
+        _this.roleConfig = [];
+        _this.isGameRunning = false;
+        _this.isMapMoved = false;
+        _this.isMapEnd = false;
+        _this.userBetData = [];
+        _this.totalCoin = INIT_TOTAL_COIN;
+        _this.betCoin = INIT_BET_AMOUNT;
+        _this.nextTimer = null;
         _this.skinName = 'resource/skins/GameView.exml';
         return _this;
     }
@@ -83,20 +55,21 @@ var GameView = (function (_super) {
         _super.prototype.createChildren.call(this);
         this.initRoles();
         this.initBetBtns();
-        this.startBtn.addEventListener('touchTap', function () {
-            this.gameMapTw.play();
-            this.roles.forEach(function (item) {
-                item.gotoAndPlay(1, -1);
-            });
-            this.rolesTimer = new egret.Timer(1000, -1);
-            this.rolesTimer.addEventListener('timer', this.rolesDash, this);
-            this.rolesTimer.start();
-        }, this);
+        // 初始化文字显示
+        this.totalCoinLabel.text = String(this.totalCoin);
+        this.betCoinLabel.text = String(this.betCoin);
+        // 绑定组件事件
+        this.addEventListener(egret.Event.ENTER_FRAME, this.onEnterFrame, this);
+        this.startBtn.addEventListener('touchTap', this.onClickStartBtn, this);
+        this.betMinusBtn.addEventListener('touchTap', this.onClickBetMinusBtn, this);
+        this.betPlusBtn.addEventListener('touchTap', this.onClickBetPlusBtn, this);
+        this.betCancelBtn.addEventListener('touchTap', this.onClickBetCancelBtn, this);
+        this.rebetBtn.addEventListener('touchTap', this.onClickRebetBtn, this);
     };
     GameView.prototype.initRoles = function () {
         var _this = this;
-        var roleConfig = RES.getRes('role_json');
-        roleConfig.forEach(function (item, index) {
+        this.roleConfig = RES.getRes('role_json');
+        this.roleConfig.forEach(function (item, index) {
             var mcData = RES.getRes(item.data);
             var mcTexture = RES.getRes(item.texture);
             var mcFactory = new egret.MovieClipDataFactory(mcData, mcTexture);
@@ -107,53 +80,195 @@ var GameView = (function (_super) {
         });
     };
     GameView.prototype.initBetBtns = function () {
-        var betBtnCollection = new eui.ArrayCollection(mapWithIndex(betBtnListConfig, {
-            amount: 0,
-            state: 'default'
-        }));
-        this.betBtnList.dataProvider = betBtnCollection;
-        this.betBtnList.addEventListener('touchTap', function (e) {
-            if (e.target.name) {
-                var index = parseInt(e.target.name);
-                var collItem = betBtnCollection.getItemAt(index);
-                betBtnCollection.replaceItemAt(__assign({}, collItem, { amount: collItem.amount + parseInt(this.betCoin.text) }), index);
-            }
-        }, this);
+        this.betBtnListConfig = RES.getRes('bet_json');
+        this.betBtnCollection = new eui.ArrayCollection(this.betBtnListConfig.slice());
+        this.betBtnList.dataProvider = this.betBtnCollection;
+        this.betBtnList.addEventListener('touchTap', this.onClickBetBtn, this);
     };
     GameView.prototype.rolesDash = function () {
-        this.roles.forEach(function (item, index) {
-            var tw = egret.Tween.get(item);
-            tw.to({
-                x: item.x - getRandomInt(0, 300)
-            }, 1000).call(function () {
-                this.onRoleTwComplete(index);
+        // 第一次动物跑到屏幕中间
+        if (!this.isMapMoved) {
+            this.roles.forEach(function (item, index) {
+                var tw = egret.Tween.get(item);
+                tw.to({
+                    x: this.stage.stageWidth / 2 + getRandomInt(-50, 50)
+                }, 1000).call(this.onRoleTwComplete.bind(this, index));
             }, this);
-        }, this);
+        }
+        else if (!this.isMapEnd) {
+            this.roles.forEach(function (item, index) {
+                var tw = egret.Tween.get(item);
+                tw.to({
+                    x: item.x + getRandomInt(-100, 100)
+                }, 1000).call(this.onRoleTwComplete.bind(this, index));
+            }, this);
+        }
+        else {
+            this.roles.forEach(function (item, index) {
+                var tw = egret.Tween.get(item);
+                var randomX = getRandomInt(-100, 100);
+                var nextX = 150 + randomX;
+                if (nextX > item.x) {
+                    nextX = 150 + getRandomInt(-100, -50);
+                }
+                tw.to({
+                    x: 150 + getRandomInt(-100, 100)
+                }, 1000).call(this.onRoleTwComplete.bind(this, index));
+            }, this);
+        }
         this.rolesTimer.stop();
     };
     GameView.prototype.onRoleTwComplete = function (index) {
+        var _this = this;
         // only want to call once
         if (index !== 0) {
             return;
         }
-        var hasWin = false;
-        var winX = [];
-        this.roles.forEach(function (item) {
-            if (item.x < 150) {
-                hasWin = true;
+        if (!this.isMapMoved) {
+            this.isMapMoved = true;
+            var tw = egret.Tween.get(this.gameMap);
+            tw.to({
+                x: 0
+            }, GAME_TIME).call(function () {
+                _this.isMapEnd = true;
+                egret.clearTimeout(_this.nextTimer);
+                _this.rolesDash();
+            });
+        }
+        var delay = this.isMapEnd ? 200 : 1000;
+        this.nextTimer = egret.setTimeout(function () {
+            if (!this.rolesTimer.running && this.isGameRunning) {
+                this.rolesTimer.start();
             }
-            winX.push(item.x);
+        }, this, delay);
+    };
+    GameView.prototype.onClickBetBtn = function (e) {
+        if (e.target.name) {
+            var index = parseInt(e.target.name);
+            var collItem = this.betBtnCollection.getItemAt(index);
+            var nextAmount = this.getNextBetAmount(collItem.amount);
+            var diffAmount = nextAmount - collItem.amount;
+            this.totalCoin -= diffAmount;
+            this.totalCoinLabel.text = String(this.totalCoin);
+            this.betBtnCollection.replaceItemAt(__assign({}, collItem, { amount: nextAmount }), index);
+        }
+    };
+    GameView.prototype.getNextBetAmount = function (a) {
+        if (a + this.betCoin <= MAX_BET_COIN) {
+            return a + this.betCoin;
+        }
+        return MAX_BET_COIN;
+    };
+    GameView.prototype.onEnterFrame = function () {
+        var _this = this;
+        if (this.isGameRunning) {
+            var hasWinRole = false;
+            var winRolesX = [];
+            this.roles.forEach(function (item, index) {
+                if (item.x < 150) {
+                    hasWinRole = true;
+                }
+                winRolesX.push(item.x);
+            }, this);
+            if (hasWinRole) {
+                this.betTopMask.fillAlpha = 0;
+                this.betPanelMask.fillAlpha = 0;
+                this.isGameRunning = false;
+                this.roles.forEach(function (item) {
+                    item.stop();
+                    egret.Tween.removeTweens(item);
+                }, this);
+                var winX = Math.min.apply(null, winRolesX);
+                var winIndex_1 = winRolesX.indexOf(winX);
+                this.roles.forEach(function (item, index) {
+                    if (index === winIndex_1) {
+                        // 更换冠军图片
+                        var collItem = _this.betBtnCollection.getItemAt(index);
+                        _this.betBtnCollection.replaceItemAt(__assign({}, collItem, { resource: collItem.active }), index);
+                        var tw = egret.Tween.get(item);
+                        tw
+                            .wait(500)
+                            .to({ alpha: 0 }).wait(500)
+                            .to({ alpha: 1 }).wait(500)
+                            .to({ alpha: 0 }).wait(500)
+                            .to({ alpha: 1 }).wait(500)
+                            .call(function () {
+                            egret.Tween.removeTweens(item);
+                            // 计算输赢金币
+                            var winCoin = 0;
+                            _this.userBetData.forEach(function (bet, index) {
+                                if (index === winIndex_1) {
+                                    winCoin += bet.amount * bet.times;
+                                }
+                            });
+                            _this.totalCoin += winCoin;
+                            _this.totalCoinLabel.text = String(_this.totalCoin);
+                            _this.roles.forEach(function (item) {
+                                item.visible = false;
+                            });
+                            var tw = egret.Tween.get(_this.gameMap);
+                            tw.to({
+                                x: -1920
+                            }, 500).call(function () {
+                                _this.roleConfig.forEach(function (item, index) {
+                                    _this.roles[index].x = item.x;
+                                    _this.roles[index].visible = true;
+                                });
+                                _this.isMapMoved = false;
+                                _this.isMapEnd = false;
+                                _this.startBtn.visible = true;
+                                _this.betBtnCollection.replaceAll(_this.betBtnListConfig.slice());
+                            });
+                            _this.betTopMask.fillAlpha = 0.5;
+                            _this.betPanelMask.fillAlpha = 0.5;
+                            _this.betTopMask.visible = false;
+                            _this.betPanelMask.visible = false;
+                        }, _this);
+                    }
+                }, this);
+            }
+        }
+    };
+    GameView.prototype.onClickStartBtn = function () {
+        this.betTopMask.visible = true;
+        this.betPanelMask.visible = true;
+        this.startBtn.visible = false;
+        this.isGameRunning = true;
+        this.userBetData = this.betBtnCollection.source.slice();
+        this.roles.forEach(function (item) {
+            item.gotoAndPlay(1, -1);
         });
-        if (hasWin) {
-            var minWinX = Math.min.apply(null, winX);
-            console.log(winX.indexOf(minWinX));
+        this.rolesTimer = new egret.Timer(1000, -1);
+        this.rolesTimer.addEventListener('timer', this.rolesDash, this);
+        this.rolesTimer.start();
+        this.rolesDash();
+    };
+    GameView.prototype.onClickBetMinusBtn = function () {
+        var i = BET_AMOUNT.indexOf(this.betCoin);
+        if (typeof BET_AMOUNT[i - 1] !== 'undefined') {
+            this.betCoin = BET_AMOUNT[i - 1];
         }
         else {
-            egret.setTimeout(function () {
-                if (!this.rolesTimer.running) {
-                    this.rolesTimer.start();
-                }
-            }, this, 1000);
+            this.betCoin = BET_AMOUNT[BET_AMOUNT.length - 1];
+        }
+        this.betCoinLabel.text = String(this.betCoin);
+    };
+    GameView.prototype.onClickBetPlusBtn = function () {
+        var i = BET_AMOUNT.indexOf(this.betCoin);
+        if (typeof BET_AMOUNT[i + 1] !== 'undefined') {
+            this.betCoin = BET_AMOUNT[i + 1];
+        }
+        else {
+            this.betCoin = BET_AMOUNT[0];
+        }
+        this.betCoinLabel.text = String(this.betCoin);
+    };
+    GameView.prototype.onClickBetCancelBtn = function () {
+        this.betBtnCollection.replaceAll(this.betBtnListConfig.slice());
+    };
+    GameView.prototype.onClickRebetBtn = function () {
+        if (this.userBetData.length) {
+            this.betBtnCollection.replaceAll(this.userBetData.slice());
         }
     };
     return GameView;
--- src/GameView.ts
+++ src/GameView.ts
@@ -1,47 +1,21 @@
-var betBtnListConfig = [
-    {
-        name: 'shizi',
-        times: 'x15',
-        resource: 'game_c1_png',
-        default: 'game_c1_png',
-        active: 'game_c1_active_png'
-    },
-    {
-        name: 'houzi',
-        times: 'x10',
-        resource: 'game_c2_png',
-        default: 'game_c2_png',
-        active: 'game_c2_active_png'
-    },
-    {
-        name: 'banma',
-        times: 'x6',
-        resource: 'game_c3_png',
-        default: 'game_c3_png',
-        active: 'game_c3_active_png'
-    },
-    {
-        name: 'luotuo',
-        times: 'x6',
-        resource: 'game_c4_png',
-        default: 'game_c4_png',
-        active: 'game_c4_active_png'
-    },
-    {
-        name: 'tuoniao',
-        times: 'x4',
-        resource: 'game_c5_png',
-        default: 'game_c5_png',
-        active: 'game_c5_active_png'
-    },
-    {
-        name: 'yezhu',
-        times: 'x4',
-        resource: 'game_c6_png',
-        default: 'game_c6_png',
-        active: 'game_c6_active_png'
-    }
-]
+interface BetData {
+    name: string,
+    times_text: string,
+    times: number,
+    amount: number,
+    index: string,
+    resource: string,
+    default: string,
+    active: string
+}
+
+interface Role {
+    name: string,
+    data: string,
+    texture: string,
+    x: number,
+    y: number
+}
 
 function mapWithIndex(arr, merge) {
     return arr.map((item, index) => {
@@ -57,43 +31,67 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
+const BET_AMOUNT = [1, 10, 20, 50, 100]
+const INIT_BET_AMOUNT = 50
+const INIT_TOTAL_COIN = 1000
+const MAX_BET_COIN = 100
+const GAME_TIME = 5000
+
 class GameView extends eui.Component {
     public constructor() {
         super()
         this.skinName = 'resource/skins/GameView.exml'
     }
 
+    // 游戏对象
     private roles: egret.MovieClip[] = []
     private rolesTimer: egret.Timer
     private betBtnList: eui.List
-    private totalCoin: eui.Label
-    private betCoin: eui.Label
+    private totalCoinLabel: eui.Label
+    private betCoinLabel: eui.Label
     private startBtn: eui.Image
     private gameMap: eui.Image
-    private gameMapTw: egret.tween.TweenGroup
+    private betBtnCollection: eui.ArrayCollection
+    private betMinusBtn: eui.Image
+    private betPlusBtn: eui.Image
+    private betTopMask: eui.Rect
+    private betPanelMask: eui.Rect
+    private betCancelBtn: eui.Image
+    private rebetBtn: eui.Image
+    
+    // 游戏数据
+    private betBtnListConfig: Object[] = []
+    private roleConfig: Role[] = []
+    private isGameRunning: boolean = false
+    private isMapMoved: boolean = false
+    private isMapEnd: boolean = false
+    private userBetData: BetData[] = []
+    private totalCoin: number = INIT_TOTAL_COIN
+    private betCoin: number = INIT_BET_AMOUNT
+    private nextTimer: number = null
 
     public childrenCreated() {
         super.createChildren()
-
         this.initRoles()
         this.initBetBtns()
 
+        // 初始化文字显示
+        this.totalCoinLabel.text = String(this.totalCoin)
+        this.betCoinLabel.text = String(this.betCoin)
 
-        this.startBtn.addEventListener('touchTap', function () {
-            this.gameMapTw.play()
-            this.roles.forEach((item) => {
-                item.gotoAndPlay(1, -1)
-            })
+        // 绑定组件事件
+        this.addEventListener(egret.Event.ENTER_FRAME, this.onEnterFrame, this)
+        this.startBtn.addEventListener('touchTap', this.onClickStartBtn, this)
+        this.betMinusBtn.addEventListener('touchTap', this.onClickBetMinusBtn, this)
+        this.betPlusBtn.addEventListener('touchTap', this.onClickBetPlusBtn, this)
+        this.betCancelBtn.addEventListener('touchTap', this.onClickBetCancelBtn, this)
+        this.rebetBtn.addEventListener('touchTap', this.onClickRebetBtn, this)
 
-            this.rolesTimer = new egret.Timer(1000, -1)
-            this.rolesTimer.addEventListener('timer', this.rolesDash, this)
-            this.rolesTimer.start()
-        }, this)
     }
 
     private initRoles(): void {
-        var roleConfig = RES.getRes('role_json')
-        roleConfig.forEach((item, index) => {
+        this.roleConfig = RES.getRes('role_json')
+        this.roleConfig.forEach((item, index) => {
             var mcData = RES.getRes(item.data)
             var mcTexture = RES.getRes(item.texture)
             var mcFactory = new egret.MovieClipDataFactory(mcData, mcTexture)
@@ -105,32 +103,45 @@ class GameView extends eui.Component {
     }
 
     private initBetBtns(): void {
-        let betBtnCollection = new eui.ArrayCollection(mapWithIndex(betBtnListConfig, {
-            amount: 0,
-            state: 'default'
-        }))
-        this.betBtnList.dataProvider = betBtnCollection
-        this.betBtnList.addEventListener('touchTap', function (e) {
-            if (e.target.name) {
-                var index = parseInt(e.target.name)
-                var collItem = betBtnCollection.getItemAt(index)
-                betBtnCollection.replaceItemAt({
-                    ...collItem,
-                    amount: collItem.amount + parseInt(this.betCoin.text)
-                }, index)
-            }
-        }, this)
+        this.betBtnListConfig = RES.getRes('bet_json')
+        this.betBtnCollection = new eui.ArrayCollection(this.betBtnListConfig.slice())
+        this.betBtnList.dataProvider = this.betBtnCollection
+        this.betBtnList.addEventListener('touchTap', this.onClickBetBtn, this)
     }
 
     private rolesDash(): void {
-        this.roles.forEach(function (item, index) {
-            var tw = egret.Tween.get(item)
-            tw.to({
-                x: item.x - getRandomInt(0, 300)
-            }, 1000).call(function () {
-                this.onRoleTwComplete(index)
+        // 第一次动物跑到屏幕中间
+        if (!this.isMapMoved) {
+            this.roles.forEach(function (item, index) {
+                var tw = egret.Tween.get(item)
+                tw.to({
+                    x: this.stage.stageWidth / 2 + getRandomInt(-50, 50)
+                }, 1000).call(this.onRoleTwComplete.bind(this, index))
             }, this)
-        }, this)
+        }
+        // 中间来回位移
+        else if (!this.isMapEnd) {
+            this.roles.forEach(function (item, index) {
+                var tw = egret.Tween.get(item)
+                tw.to({
+                    x: item.x + getRandomInt(-100, 100)
+                }, 1000).call(this.onRoleTwComplete.bind(this, index))
+            }, this)
+        }
+        // 最后冲刺
+        else {
+            this.roles.forEach(function (item, index) {
+                var tw = egret.Tween.get(item)
+                var randomX = getRandomInt(-100, 100)
+                var nextX = 150 + randomX
+                if (nextX > item.x) {
+                    nextX = 150 + getRandomInt(-100, -50)
+                }
+                tw.to({
+                    x: 150 + getRandomInt(-100, 100)
+                }, 1000).call(this.onRoleTwComplete.bind(this, index))
+            }, this)
+        }
         this.rolesTimer.stop()
     }
 
@@ -139,24 +150,182 @@ class GameView extends eui.Component {
         if (index !== 0) {
             return
         }
-        var hasWin = false
-        var winX = []
-        this.roles.forEach(function (item) {
-            if (item.x < 150) {
-                hasWin = true
+
+        if (!this.isMapMoved) {
+            this.isMapMoved = true
+
+            let tw = egret.Tween.get(this.gameMap)
+            tw.to({
+                x: 0
+            }, GAME_TIME).call(() => {
+                this.isMapEnd = true
+                egret.clearTimeout(this.nextTimer)
+                this.rolesDash()
+            })
+        }
+
+        let delay = this.isMapEnd ? 200 : 1000
+
+        this.nextTimer = egret.setTimeout(function () {
+            if (!this.rolesTimer.running && this.isGameRunning) {
+                this.rolesTimer.start()
             }
-            winX.push(item.x)
+        }, this, delay)
+    }
+
+    private onClickBetBtn(e: egret.Event): void {
+        if (e.target.name) {
+            var index = parseInt(e.target.name)
+            var collItem = this.betBtnCollection.getItemAt(index)
+            var nextAmount = this.getNextBetAmount(collItem.amount)
+            var diffAmount = nextAmount - collItem.amount
+            this.totalCoin -= diffAmount
+            this.totalCoinLabel.text = String(this.totalCoin)
+
+            this.betBtnCollection.replaceItemAt({
+                ...collItem,
+                amount: nextAmount
+            }, index)
+        }
+    }
+
+    private getNextBetAmount(a) {
+        if (a + this.betCoin <= MAX_BET_COIN) {
+            return a + this.betCoin
+        }
+        return MAX_BET_COIN
+    }
+
+    private onEnterFrame(): void {
+        if (this.isGameRunning) {
+            var hasWinRole = false
+            var winRolesX: number[] = []
+            this.roles.forEach(function (item, index) {
+                if (item.x < 150) {
+                    hasWinRole = true
+                }
+                winRolesX.push(item.x)
+            }, this)
+
+            if (hasWinRole) {
+                this.betTopMask.fillAlpha = 0
+                this.betPanelMask.fillAlpha = 0
+                this.isGameRunning = false
+                this.roles.forEach(function (item) {
+                    item.stop()
+                    egret.Tween.removeTweens(item)
+                }, this)
+
+                let winX = Math.min.apply(null, winRolesX)
+                let winIndex = winRolesX.indexOf(winX)
+
+                this.roles.forEach((item, index) => {
+                    if (index === winIndex) {
+                        // 更换冠军图片
+                        var collItem = this.betBtnCollection.getItemAt(index)
+                        this.betBtnCollection.replaceItemAt({
+                            ...collItem,
+                            resource: collItem.active
+                        }, index)
+
+                        let tw = egret.Tween.get(item)
+                        tw
+                        .wait(500)
+                        .to({ alpha: 0 }).wait(500)
+                        .to({ alpha: 1 }).wait(500)
+                        .to({ alpha: 0 }).wait(500)
+                        .to({ alpha: 1 }).wait(500)
+                        .call(() => {
+                            egret.Tween.removeTweens(item)
+
+                            // 计算输赢金币
+                            var winCoin = 0
+                            this.userBetData.forEach(function(bet, index) {
+                                if (index === winIndex) {
+                                    winCoin += bet.amount * bet.times
+                                }
+                            })
+                            this.totalCoin += winCoin
+                            this.totalCoinLabel.text = String(this.totalCoin)
+
+                            
+
+                            this.roles.forEach((item) => {
+                                item.visible = false
+                            })
+
+                            let tw = egret.Tween.get(this.gameMap)
+                            tw.to({
+                                x: -1920
+                            }, 500).call(() => {
+                                this.roleConfig.forEach((item, index) => {
+                                    this.roles[index].x = item.x
+                                    this.roles[index].visible = true
+                                })
+                                this.isMapMoved = false
+                                this.isMapEnd = false
+                                this.startBtn.visible = true
+                                this.betBtnCollection.replaceAll(this.betBtnListConfig.slice())
+                            })
+                        
+                            this.betTopMask.fillAlpha = 0.5
+                            this.betPanelMask.fillAlpha = 0.5
+                            this.betTopMask.visible = false
+                            this.betPanelMask.visible = false
+                        }, this)
+                    }
+                }, this)
+            }
+        }
+    }
+
+    private onClickStartBtn(): void {
+        this.betTopMask.visible = true
+        this.betPanelMask.visible = true
+        this.startBtn.visible = false
+        this.isGameRunning = true
+
+        this.userBetData = this.betBtnCollection.source.slice()
+
+        this.roles.forEach((item) => {
+            item.gotoAndPlay(1, -1)
         })
-        if (hasWin) {
-            var minWinX = Math.min.apply(null, winX)
-            console.log(winX.indexOf(minWinX))
+
+        this.rolesTimer = new egret.Timer(1000, -1)
+        this.rolesTimer.addEventListener('timer', this.rolesDash, this)
+        this.rolesTimer.start()
+        this.rolesDash()
+    }
+
+    private onClickBetMinusBtn(): void {
+        var i = BET_AMOUNT.indexOf(this.betCoin)
+        if (typeof BET_AMOUNT[i - 1] !== 'undefined') {
+            this.betCoin = BET_AMOUNT[i - 1]
         }
         else {
-            egret.setTimeout(function () {
-                if (!this.rolesTimer.running) {
-                    this.rolesTimer.start()
-                }
-            }, this, 1000)
+            this.betCoin = BET_AMOUNT[BET_AMOUNT.length - 1]
+        }
+        this.betCoinLabel.text = String(this.betCoin)
+    }
+
+    private onClickBetPlusBtn(): void {
+        var i = BET_AMOUNT.indexOf(this.betCoin)
+        if (typeof BET_AMOUNT[i + 1] !== 'undefined') {
+            this.betCoin = BET_AMOUNT[i + 1]
+        }
+        else {
+            this.betCoin = BET_AMOUNT[0]
+        }
+        this.betCoinLabel.text = String(this.betCoin)
+    }
+
+    private onClickBetCancelBtn():void {
+        this.betBtnCollection.replaceAll(this.betBtnListConfig.slice())
+    }
+
+    private onClickRebetBtn(): void {
+        if (this.userBetData.length) {
+            this.betBtnCollection.replaceAll(this.userBetData.slice())
         }
     }
 
